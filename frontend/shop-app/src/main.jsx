f961ddc8d853e36e876644e6b2854b0066c70f20
Szymi611 <szymondomagala32@gmail.com>
2024-10-25 19:05:54 +0200

    Trying to fix styles

--- frontend/shop-app/src/main.jsx
+++ frontend/shop-app/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import './style_z_index_przedTailwind.css'
+//import './style_z_index_przedTailwind.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
