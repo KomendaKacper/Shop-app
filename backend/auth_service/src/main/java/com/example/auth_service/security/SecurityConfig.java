36ac2ce7679adc94a518fe2f6370ede64860c280
KomendaKacper <komenda@student.agh.edu.pl>
2024-10-26 15:25:41 +0200

    Password reset

--- backend/auth_service/src/main/java/com/example/auth_service/security/SecurityConfig.java
+++ backend/auth_service/src/main/java/com/example/auth_service/security/SecurityConfig.java
@@ -1,14 +1,8 @@
 package com.example.auth_service.security;
 
-import com.example.auth_service.entity.Role;
-import com.example.auth_service.entity.RolesEnum;
-import com.example.auth_service.entity.User;
 import com.example.auth_service.jwt.AuthEntryPointJwt;
 import com.example.auth_service.jwt.AuthTokenFilter;
-import com.example.auth_service.repositories.RoleRepository;
-import com.example.auth_service.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -22,8 +16,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
-import java.time.LocalDate;
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -32,12 +24,12 @@ public class SecurityConfig {
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf ->
                 csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                        .ignoringRequestMatchers("/api/auth/**"));
+                        .ignoringRequestMatchers("/api/auth/public/**"));
 
         http.authorizeHttpRequests(request -> request
                         .requestMatchers("/api/admin/**").hasRole("ADMIN")
                         .requestMatchers("/api/csrf-token/**").permitAll()
-                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/auth/public/**").permitAll()
                         .anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
@@ -64,44 +56,4 @@ public class SecurityConfig {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
-    @Bean
-    public CommandLineRunner initData(RoleRepository roleRepository, UserRepository userRepository) {
-        return args -> {
-            System.out.println("CommandLineRunner is running...");
-
-            Role userRole = roleRepository.findByRoleName(RolesEnum.ROLE_USER)
-                    .orElseGet(() -> roleRepository.save(new Role(RolesEnum.ROLE_USER)));
-
-            Role adminRole = roleRepository.findByRoleName(RolesEnum.ROLE_ADMIN)
-                    .orElseGet(() -> roleRepository.save(new Role(RolesEnum.ROLE_ADMIN)));
-
-            if (!userRepository.existsByUserName("user1")) {
-                User user1 = new User("user1", "user1@example.com", passwordEncoder().encode("password1"));
-                user1.setAccountNonLocked(false);
-                user1.setAccountNonExpired(true);
-                user1.setCredentialsNonExpired(true);
-                user1.setEnabled(true);
-                user1.setCredentialsExpiryDate(LocalDate.now().plusYears(1));
-                user1.setAccountExpiryDate(LocalDate.now().plusYears(1));
-                user1.setTwoFactorEnabled(false);
-                user1.setSignUpMethod("email");
-                user1.setRole(userRole);
-                userRepository.save(user1);
-            }
-
-            if (!userRepository.existsByUserName("admin")) {
-                User admin = new User("admin", "admin@example.com", passwordEncoder().encode("adminPass"));
-                admin.setAccountNonLocked(true);
-                admin.setAccountNonExpired(true);
-                admin.setCredentialsNonExpired(true);
-                admin.setEnabled(true);
-                admin.setCredentialsExpiryDate(LocalDate.now().plusYears(1));
-                admin.setAccountExpiryDate(LocalDate.now().plusYears(1));
-                admin.setTwoFactorEnabled(false);
-                admin.setSignUpMethod("email");
-                admin.setRole(adminRole);
-                userRepository.save(admin);
-            }
-        };
-    }
 }
