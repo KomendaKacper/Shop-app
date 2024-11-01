f80cc01eec8e400a35c371bb1a04882e84edc286
Szymi611 <szymondomagala32@gmail.com>
2024-11-01 20:51:23 +0100

    Adding fetch clothes with basic style with big contrast just to check

--- frontend/shop-app/src/App.jsx
+++ frontend/shop-app/src/App.jsx
@@ -8,32 +8,32 @@ function App() {
   const [clothes, setClothes] = useState([]);
   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    async function fetchClothes() {
-      try {
-        console.log("Zaczynam pobieranie danych");
-        const response = await fetch("http://localhost:8081/api/clotheses");
-        console.log("Koniec pobierania danych");
+  // useEffect(() => {
+  //   async function fetchClothes() {
+  //     try {
+  //       console.log("Zaczynam pobieranie danych");
+  //       const response = await fetch("http://localhost:8081/api/clotheses");
+  //       console.log("Koniec pobierania danych");
 
-        if (!response.ok) {
-          setError("Error fetching data");
-          return;
-        }
+  //       if (!response.ok) {
+  //         setError("Error fetching data");
+  //         return;
+  //       }
 
-        const data = await response.json();
-        setClothes(data._embedded.clotheses);
-        console.log("Ubrania: ", data._embedded.clotheses);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-    //fetchClothes();
-  }, []);
+  //       const data = await response.json();
+  //       setClothes(data._embedded.clotheses);
+  //       console.log("Ubrania: ", data._embedded.clotheses);
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   }
+  //   //fetchClothes();
+  // }, []);
 
   return (
     <>
       <Navbar />
-      <Clothes clothes={clothes}/>
+      <Clothes/>
     </>
   )
 }
