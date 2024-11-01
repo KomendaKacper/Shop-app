e4ef8e52c81b910641f0a38ae133d0b7be25eca6
Szymi611 <szymondomagala32@gmail.com>
2024-11-01 20:59:17 +0100

    Fixing prop name

--- frontend/shop-app/src/components/ClothesItem.jsx
+++ frontend/shop-app/src/components/ClothesItem.jsx
@@ -2,7 +2,7 @@ export default function ClothesItem({ product }) {
   return(
     <li className="w-[20rem] bg-[rgb(176,159,139)]  rounded-2xl overflow-hidden text-center shadow-lg">
       <article className="h-full flex flex-col justify-between">
-        <img src={product.image} alt={product.name}  className="w-full h-80 object-cover"/>
+        <img src={product.imageUrl} alt={product.name}  className="w-full h-80 object-cover"/>
         <div>
           <h3 className="text-2xl font-[bold] mx-0 my-3">{product.name}</h3>
           <p className="inline-block bg-[rgb(97,87,74)] text-[#ffc404] text-[0.9rem] font-[bold] rounded m-0 px-8 py-2 hover:bg-[rgb(82,74,63)] cursor-pointer">{product.price}</p>
