a976b0147267f61b3dadc5573e92ddde629da02d
Szymi611 <szymondomagala32@gmail.com>
2024-11-01 20:57:57 +0100

    Adding styled clothes component

--- frontend/shop-app/src/App.jsx
+++ frontend/shop-app/src/App.jsx
@@ -2,33 +2,9 @@
 import Clothes from './components/Clothes'
 import Navbar from './components/Navbar';
 
-import { useEffect, useState } from 'react'
+
 
 function App() {
-  const [clothes, setClothes] = useState([]);
-  const [error, setError] = useState(null);
-
-  // useEffect(() => {
-  //   async function fetchClothes() {
-  //     try {
-  //       console.log("Zaczynam pobieranie danych");
-  //       const response = await fetch("http://localhost:8081/api/clotheses");
-  //       console.log("Koniec pobierania danych");
-
-  //       if (!response.ok) {
-  //         setError("Error fetching data");
-  //         return;
-  //       }
-
-  //       const data = await response.json();
-  //       setClothes(data._embedded.clotheses);
-  //       console.log("Ubrania: ", data._embedded.clotheses);
-  //     } catch (error) {
-  //       console.error(error);
-  //     }
-  //   }
-  //   //fetchClothes();
-  // }, []);
 
   return (
     <>
--- frontend/shop-app/src/components/ClothesItem.jsx
+++ frontend/shop-app/src/components/ClothesItem.jsx
@@ -1,15 +1,15 @@
 export default function ClothesItem({ product }) {
   return(
-    <li className="w-[20rem] bg-black rounded-2xl overflow-hidden text-center shadow-sm">
+    <li className="w-[20rem] bg-[rgb(176,159,139)]  rounded-2xl overflow-hidden text-center shadow-lg">
       <article className="h-full flex flex-col justify-between">
         <img src={product.image} alt={product.name}  className="w-full h-80 object-cover"/>
         <div>
           <h3 className="text-2xl font-[bold] mx-0 my-3">{product.name}</h3>
-          <p className="inline-block bg-[#312c1d] text-[#ffc404] text-[0.9rem] font-[bold] rounded m-0 px-8 py-2">{product.price}</p>
+          <p className="inline-block bg-[rgb(97,87,74)] text-[#ffc404] text-[0.9rem] font-[bold] rounded m-0 px-8 py-2 hover:bg-[rgb(82,74,63)] cursor-pointer">{product.price}</p>
           <p className="mb-4">{product.description}</p> 
         </div>
         <p className="mb-6">
-          <button>Add to Cart</button>
+          <button className="bg-[rgb(97,87,74)] hover:bg-[rgb(82,74,63)] cursor-pointer rounded-md h-8 w-[8rem]">Add to Cart</button>
         </p>
       </article>
     </li>
