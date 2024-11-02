876f13044d41ec62de204baf9c2678641a5bb796
Szymi611 <szymondomagala32@gmail.com>
2024-11-02 10:44:55 +0100

    Small changes with styling

--- frontend/shop-app/src/components/ClothesItem.jsx
+++ frontend/shop-app/src/components/ClothesItem.jsx
@@ -6,7 +6,7 @@ export default function ClothesItem({ product }) {
         <div>
           <h3 className="text-2xl font-[bold] mx-0 my-3">{product.name}</h3>
           <p className="inline-block bg-[rgb(97,87,74)] text-[#ffc404] text-[0.9rem] font-[bold] rounded m-0 px-8 py-2 hover:bg-[rgb(82,74,63)] cursor-pointer">{product.price}</p>
-          <p className="mb-4">{product.description}</p> 
+          <p className="mb-2 mt-2">{product.description}</p> 
         </div>
         <p className="mb-6">
           <button className="bg-[rgb(97,87,74)] hover:bg-[rgb(82,74,63)] cursor-pointer rounded-md h-8 w-[8rem]">Add to Cart</button>
