ce1312eda9e5c3bceb40d64ddfa6f797b0f4e604
KomendaKacper <komenda@student.agh.edu.pl>
2024-12-02 16:07:05 +0100

    Setting clothes photos

--- backend/catalog_service/src/main/java/com/example/online_store/services/DataInitializer.java
+++ backend/catalog_service/src/main/java/com/example/online_store/services/DataInitializer.java
@@ -16,27 +16,27 @@ public class DataInitializer {
     @PostConstruct
     public void init() {
         if (!clothesRepository.existsByName("T-shirt")) {
-            Clothes clothes1 = new Clothes("T-shirt", "Stylish t-shirt", "M", "Black", 49.99, "T-shirt", "clothesImages/jacket.jpg");
+            Clothes clothes1 = new Clothes("T-shirt", "Stylish t-shirt", "M", "Black", 49.99, "T-shirt", "src/main/resources/static/clothesImages/jacket.jpg");
             clothesRepository.save(clothes1);
         }
 
         if (!clothesRepository.existsByName("Trousers")) {
-            Clothes clothes2 = new Clothes("Trousers", "Comfortable trousers", "L", "Blue", 89.99, "Trousers", "clothesImages/trousers.jpg");
+            Clothes clothes2 = new Clothes("Trousers", "Comfortable trousers", "L", "Blue", 89.99, "Trousers", "src/main/resources/static/clothesImages/trousers.jpg");
             clothesRepository.save(clothes2);
         }
 
         if (!clothesRepository.existsByName("Hat")) {
-            Clothes clothes3 = new Clothes("Hat", "Nice hat", "One size", "Beage", 49.99, "Hat", "clothesImages/hat.jpg");
+            Clothes clothes3 = new Clothes("Hat", "Nice hat", "One size", "Beage", 49.99, "Hat", "src/main/resources/static/clothesImages/hat.jpg");
             clothesRepository.save(clothes3);
         }
 
         if (!clothesRepository.existsByName("Shirt")) {
-            Clothes clothes4 = new Clothes("Shirt", "Basic Shirt", "S", "White", 69.99, "Shirt", "clothesImages/shirt.jpg");
+            Clothes clothes4 = new Clothes("Shirt", "Basic Shirt", "S", "White", 69.99, "Shirt", "src/main/resources/static/clothesImages/shirt.jpg");
             clothesRepository.save(clothes4);
         }
 
         if (!clothesRepository.existsByName("Another Shirt")) {
-            Clothes clothes5 = new Clothes("Another Shirt", "Not really a basic shirt", "M", "White", 69.99, "Shirt", "clothesImages/shirt2.jpg");
+            Clothes clothes5 = new Clothes("Another Shirt", "Not really a basic shirt", "M", "White", 69.99, "Shirt", "src/main/resources/static/clothesImages/shirt2.jpg");
             clothesRepository.save(clothes5);
         }
 
