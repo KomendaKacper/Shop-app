c13cdea5d3e0cbcb8faf2a58e75fc56b802f2ee3
Szymi611 <szymondomagala32@gmail.com>
2024-12-05 19:49:28 +0100

    Adding authorization useState

--- frontend/shop-app/src/components/Checkout.jsx
+++ frontend/shop-app/src/components/Checkout.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import Modal from "./UI/Modal";
 import CartContext from "../store/CartContext";
 import Input from "./UI/Input";
@@ -17,32 +17,59 @@ export default function Checkout() {
     userProgressCtx.hideCheckout();
   }
 
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    street: "",
+    postalCode: "",
+    city: "",
+  });
+
+  const handleInputChange = (e) => {
+    const {name, value} = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    })
+  }
+
+  function test(){
+    console.log(formData);
+  }
+
   return (
     <Modal open={userProgressCtx.progress === "checkout"}>
-      <form action="">
-        <h2>Checkout</h2>
-        <p>Total Amount: {cartTotal.toFixed(2)}</p>
+      <form className=" " onSubmit={test}>
+        <h2 className="justify-center flex text-2xl m-4" onClick={handleClose}>
+          Checkout
+        </h2>
 
-        <Input label="Name" id="name" type="text" />
-        <Input label="Email" id="email" type="email" />
-        <Input label="Street" id="street" type="text" />
+        <Input label="Name" id="name" type="text" value={formData.name} onChange={handleInputChange}/>
+        <Input label="Email" id="email" type="email" value={formData.email} onChange={handleInputChange}/>
+        <Input label="Street" id="street" type="text" value={formData.street} onChange={handleInputChange}/>
         <div className="flex justify-start g-4">
-          <Input label="Postal Code" id="postal-code" type="text" />
-          <Input label="City" id="city" type="text" />
+          <Input label="Postal Code" id="postalCode" type="text" value={formData.postalCode} onChange={handleInputChange}/>
+          <Input label="City" id="city" type="text" value={formData.city} onChange={handleInputChange}/>
         </div>
-
-        <p className="flex justify-end g-4">
-          <button
-            onClick={handleClose}
-            type="button"
-            className="rounded-xl mr-2 ml-1 text-m py-2 px-4 cursor-pointer"
-          >
-            Close
-          </button>
-          <button className="rounded-xl mr-2 ml-1 text-m py-2 px-4 ">
-            Submit purchase
-          </button>
-        </p>
+        <div className="mb-12">
+          <p className="float-right mt-1 mb-1 p-2">
+            <b>Total Amount: {cartTotal.toFixed(2)} $</b>
+          </p>
+        </div>
+        <div>
+          <p className="float-right m-1 p-1">
+            <button
+              onClick={handleClose}
+              type="button"
+              className="rounded-xl mr-2 ml-1 text-m py-1 px-4 cursor-pointer hover:bg-[#33312e57]"
+            >
+              Close
+            </button>
+            <button className="rounded-xl ml-2  bg-[#FFBF00] text-m py-1 px-4 cursor-pointer hover:bg-[#ffab04] ">
+              Submit purchase
+            </button>
+          </p>
+        </div>
       </form>
     </Modal>
   );
