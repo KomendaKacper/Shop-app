23ce351d74245ee7188f17f4188da3bc2a609be9
Szymi611 <szymondomagala32@gmail.com>
2025-01-10 17:37:23 +0100

    Fixing checkout styles

--- frontend/shop-app/src/components/Checkout.jsx
+++ frontend/shop-app/src/components/Checkout.jsx
@@ -26,49 +26,80 @@ export default function Checkout() {
   });
 
   const handleInputChange = (e) => {
-    const {name, value} = e.target;
+    const { name, value } = e.target;
     setFormData({
       ...formData,
       [name]: value,
-    })
-  }
+    });
+  };
 
-  function test(){
+  function test() {
     console.log(formData);
   }
 
   return (
     <Modal open={userProgressCtx.progress === "checkout"}>
-      <form className=" " onSubmit={test}>
+      <form className="flex flex-col" onSubmit={test}>
         <h2 className="justify-center flex text-2xl m-4" onClick={handleClose}>
-          Checkout
+          <b>Checkout</b>
         </h2>
-
-        <Input label="Name" id="name" type="text" value={formData.name} onChange={handleInputChange}/>
-        <Input label="Email" id="email" type="email" value={formData.email} onChange={handleInputChange}/>
-        <Input label="Street" id="street" type="text" value={formData.street} onChange={handleInputChange}/>
-        <div className="flex justify-start g-4">
-          <Input label="Postal Code" id="postalCode" type="text" value={formData.postalCode} onChange={handleInputChange}/>
-          <Input label="City" id="city" type="text" value={formData.city} onChange={handleInputChange}/>
-        </div>
-        <div className="mb-12">
-          <p className="float-right mt-1 mb-1 p-2">
-            <b>Total Amount: {cartTotal.toFixed(2)} $</b>
-          </p>
-        </div>
-        <div>
-          <p className="float-right m-1 p-1">
-            <button
-              onClick={handleClose}
-              type="button"
-              className="rounded-xl mr-2 ml-1 text-m py-1 px-4 cursor-pointer hover:bg-[#33312e57]"
-            >
-              Close
-            </button>
-            <button className="rounded-xl ml-2  bg-[#FFBF00] text-m py-1 px-4 cursor-pointer hover:bg-[#ffab04] ">
-              Submit purchase
-            </button>
-          </p>
+        <div className="w-[600px] h-max">
+          <Input
+            label="Name"
+            id="name"
+            type="text"
+            value={formData.name}
+            onChange={handleInputChange}
+          />
+          <Input
+            label="Email"
+            id="email"
+            type="email"
+            value={formData.email}
+            onChange={handleInputChange}
+          />
+          <Input
+            label="Street"
+            id="street"
+            type="text"
+            value={formData.street}
+            onChange={handleInputChange}
+          />
+          <div className="flex justify-start g-4">
+            <Input
+              label="Postal Code"
+              id="postalCode"
+              type="text"
+              value={formData.postalCode}
+              onChange={handleInputChange}
+            />
+            <Input
+              label="City"
+              id="city"
+              type="text"
+              value={formData.city}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="mb-12">
+            <p className="float-right mt-1 mb-1 p-2">
+              <b>Total Amount: {cartTotal.toFixed(2)} $</b>
+            </p>
+          </div>
+          <div>
+            <p className="float-right m-1 p-1">
+              <button
+                onClick={handleClose}
+                type="button"
+                className="rounded-xl mr-2 ml-1 text-m py-1 px-4 cursor-pointer hover:bg-[#33312e57]"
+              >
+                Close
+              </button>
+              <button className="rounded-xl ml-2  bg-[#FFBF00] text-m py-1 px-4 cursor-pointer hover:bg-[#ffab04] ">
+                Submit purchase
+              </button>
+            </p>
+          </div>
         </div>
       </form>
     </Modal>
