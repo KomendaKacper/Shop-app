62ddb2f0b039394f9d651c35aad52eb0be3ea231
Szymi611 <szymondomagala32@gmail.com>
2025-01-10 18:14:13 +0100

    Adding logged in status and logout logic

--- frontend/shop-app/src/components/Navbar.jsx
+++ frontend/shop-app/src/components/Navbar.jsx
@@ -5,24 +5,30 @@ import "../index.css";
 import { useContext } from "react";
 import CartContext from "../store/CartContext";
 import UserProgressContext from "../store/UserProgressContext";
+import { IoIosLogOut } from "react-icons/io"; //LogOut icon
+import  AuthContext  from "../store/AuthContext";
 
 export default function Navbar() {
   const cartCtx = useContext(CartContext);
   const userProgressCtx = useContext(UserProgressContext);
+  const authCtx = useContext(AuthContext);
 
   const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
     return totalNumberOfItems + item.quantity;
-  }, 0)
+  }, 0);
 
-  function handleShowCart(){
-    userProgressCtx.showCart()
+  function handleShowCart() {
+    userProgressCtx.showCart();
   }
 
   return (
     <div id="nav-container" className="p-5 ml-6 mr-6 ">
       <nav className="flex justify-between items-center">
         <h1 id="nav-header" className="text-[2rem] m-0 float-left">
-          <a className=" no-underline text-white hover:text-[rgb(156,140,121)]" href="/">
+          <a
+            className=" no-underline text-white hover:text-[rgb(156,140,121)]"
+            href="/"
+          >
             Clothes Shop
           </a>
         </h1>
@@ -31,23 +37,35 @@ export default function Navbar() {
             id="nav-el"
             className=" ml-[20px] mr-[20px] cursor-pointer float-right hover:text-[rgb(183,164,143)] hover:scale-150"
           >
-            <a href="/home"><IoHome size={25} /></a>
+            <a href="/home">
+              <IoHome size={25} />
+            </a>
           </li>
           <li
             id="nav-el"
             className="ml-[20px] mr-[20px] cursor-pointer float-right hover:text-[rgb(183,164,143)] hover:scale-150"
           >
-            <a href="/login"><CgProfile size={25} /></a>
+            {console.log(authCtx.isLoggedIn)}
+            {authCtx.isLoggedIn ? (
+              <a>
+                <IoIosLogOut size={25} onClick={authCtx.logout}/>
+              </a>
+            ) : (
+              <a>
+                <CgProfile size={25} onClick={authCtx.login}/>
+              </a>
+            )}
           </li>
           <li
             id="nav-el"
             className="ml-[20px] mr-[20px] cursor-pointer float-right hover:text-[rgb(183,164,143)] hover:scale-150 flex items-center"
           >
-            <FaCartShopping size={25} onClick={handleShowCart}/> <p className="ml-1 text-l">({totalCartItems})</p>
+            <FaCartShopping size={25} onClick={handleShowCart} />{" "}
+            <p className="ml-1 text-l">({totalCartItems})</p>
           </li>
         </ul>
       </nav>
-      <hr className="mt-4"/>
+      <hr className="mt-4" />
     </div>
   );
 }
