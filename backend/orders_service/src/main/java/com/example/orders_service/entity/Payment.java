28f0eb27e22b1b3beb2c1a63a77e33ade5e24d70
KomendaKacper <komenda@student.agh.edu.pl>
2025-01-14 21:10:08 +0100

    Stripe payment setup

---
+++ backend/orders_service/src/main/java/com/example/orders_service/entity/Payment.java
@@ -0,0 +1,21 @@
+package com.example.orders_service.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "payment")
+@Data
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "user_email")
+    private String userEmail;
+
+    @Column(name = "amount")
+    private double amount;
+}
