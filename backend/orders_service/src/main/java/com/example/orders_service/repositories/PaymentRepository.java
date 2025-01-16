96872e3e3cac51c631e8497a422e8779b7615d3f
KomendaKacper <komenda@student.agh.edu.pl>
2025-01-16 16:28:57 +0100

    Secret managing

--- backend/orders_service/src/main/java/com/example/orders_service/repositories/PaymentRepository.java
+++ backend/orders_service/src/main/java/com/example/orders_service/repositories/PaymentRepository.java
@@ -2,7 +2,9 @@ package com.example.orders_service.repositories;
 
 import com.example.orders_service.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
 
     Payment findByUserEmail(String userEmail);
