23980e883ed14c1a75a9e13926c194fa3f2432ef
KomendaKacper <komenda@student.agh.edu.pl>
2025-02-17 00:07:02 +0100

    Up

--- backend/catalog_service/src/test/java/com/example/online_store/tests/ClothesControllerTest.java
+++ backend/catalog_service/src/test/java/com/example/online_store/tests/ClothesControllerTest.java
@@ -1,2 +1,40 @@
-package com.example.online_store.tests;public class ClothesControllerTest {
+package com.example.online_store.tests;
+
+import com.example.online_store.entity.Clothes;
+import com.example.online_store.services.ClothesService;
+import org.junit.jupiter.api.BeforeAll;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+@TestPropertySource("/application-test.properties")
+@AutoConfigureMockMvc
+@SpringBootTest
+@Transactional
+public class ClothesControllerTest {
+
+    private static MockHttpServletRequest request;
+
+    @Mock
+    ClothesService clothesService;
+
+    Clothes clothes;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private JdbcTemplate jdbc;
+
+    @BeforeAll
+    public static void setup() {
+        request = new MockHttpServletRequest();
+    }
+
 }
