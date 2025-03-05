fe2591b11269f33cefdecefcfa188a046d0a38f6
KomendaKacper <komenda@student.agh.edu.pl>
2025-03-05 17:42:19 +0100

    Csrf token auth

--- backend/catalog_service/src/test/java/com/example/online_store/tests/ClothesControllerTest.java
+++ backend/catalog_service/src/test/java/com/example/online_store/tests/ClothesControllerTest.java
@@ -8,10 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
